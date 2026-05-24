c88e5df711730cad554cbf0998c306309cc499b7
ankitkumar-22 <121615440+ankitkumar-22@users.noreply.github.com>
2026-05-24 11:48:28 +0530

    bipartite update

--- bipartite.java
+++ bipartite.java
@@ -1,23 +1,68 @@
 //link : https://leetcode.com/problems/is-graph-bipartite/
 
-
+// Solution using DFS
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int[] color = new int[graph.length];
-        for(int i =0 ;i<graph.length;i++)
-        {
-            if(color[i]== 0) 
-                if(!dfs(graph,i,color,1)) return false; 
+        int n = graph.length;
+        int[] color = new int[n];
+        for(int i = 0;i<n;i++){
+            if(color[i]==0){
+                color[i] = 1;
+                if(!dfs(graph,i,color))
+                    return false;
+            }
         }
-        return true;                   
+        return true;
     }
-    public boolean dfs(int[][] graph,int node , int[] color, int current_color){
-        color[node] = current_color;
-        int next_color = current_color==1?2:1;
-        for(int neighbour : graph[node]){
-            if(color[neighbour] == 0)
-                if(!dfs(graph,neighbour,color,next_color)) return false;
-            if(color[neighbour]== current_color) return false;
+    public boolean dfs(int[][] graph, int node, int[] color){
+        int currColor = color[node];
+        int nextColor = (currColor==1)?2:1;
+        for(int nextNode: graph[node]){
+            if(color[nextNode]==0){
+                color[nextNode] = nextColor;
+                if(!dfs(graph,nextNode,color))
+                    return false;
+            }else if(color[nextNode]==currColor){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+//Solution using BFS
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] vis = new int[n];
+        
+        for(int i = 0;i<n;i++){
+            if(vis[i]==0){
+                vis[i] = 1;
+                if(!bfs(graph,i,vis))
+                    return false;
+            }
+        }
+        return true;
+    }
+    public boolean bfs(int[][] graph, int node, int[] vis){
+        vis[node] = 1;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(node);
+        while(!queue.isEmpty()){
+            int curr = queue.poll();
+            int currColor = vis[curr];
+            int nextColor = (currColor==1)?2:1;
+            for(int next : graph[curr]){
+                if(vis[next]==0){
+                    vis[next] = nextColor;
+                    queue.offer(next);
+                }else{
+                    if(vis[next]==currColor)
+                        return false;
+                }
+            }
         }
         return true;
     }

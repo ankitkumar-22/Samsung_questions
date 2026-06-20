d1798cf78c871013a17b105d5b4154f23315bd47
ankitkumar-22 <121615440+ankitkumar-22@users.noreply.github.com>
2026-06-20 20:11:11 +0530

    tiles

--- tiles.java
+++ tiles.java
@@ -72,3 +72,82 @@ class Solution{
         return false;
     }
 }
+
+
+
+
+//same logic cleaner code
+import java.util.*;
+import java.io.*;
+public class Samsung {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder res;
+
+    static int[][] tiles;
+    static int n;
+    static int k;
+    public static void main(String[] args) throws IOException{
+        br= new BufferedReader(new InputStreamReader(System.in));
+        int t = 1;
+        res = new StringBuilder();
+        while(t-->0){
+            fn();
+        }
+        System.out.println(res);
+    }
+    public static void fn() throws IOException{
+        n = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
+        tiles = new int[n][2];
+        for(int i = 0; i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            tiles[i][0] = Integer.parseInt(st.nextToken());
+            tiles[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(tiles,(a,b)->Integer.compare(a[0],b[0]));
+
+
+        int left = 0;
+        int right = (int)1e9;
+        int ans = -1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(helper(mid)){
+                ans = mid;
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        res.append(ans);
+    }
+    public static boolean helper(int minDiff) {
+        int left = 0;
+        for(int right = 0;right<n;right++){
+            while(tiles[right][0]-tiles[left][0]>minDiff)
+                left++;
+            if(right-left+1>=k){
+                List<int[]> temp = new ArrayList<>();
+                for(int i = left; i<=right; i++)
+                    temp.add(tiles[i]);
+                if(subHelper(temp,minDiff))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean subHelper(List<int[]> temp, int minDiff){
+        temp.sort((a,b)->Integer.compare(a[1],b[1]));
+        int left = 0;
+        for(int right = 0; right<temp.size();right++){
+            while(temp.get(right)[1]-temp.get(left)[1]>minDiff)
+                left++;
+            if(right-left+1>=k)
+                return true;
+        }
+        return false;
+    }
+}
+
